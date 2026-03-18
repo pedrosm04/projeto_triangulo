ddf2c79812992c10703f11acaf6002695c1740e6
pepes <pepes@pepmedeiros>
2026-03-17 21:14:29 -0300

    fix: comicio de linhas

--- src/main/java/principal.java
+++ src/main/java/principal.java
@@ -6,10 +6,14 @@ import javax.swing.JOptionPane;
 public class principal {
 
     public static void main(String[] args) {
+        //variaveis
         double base,altura,area;
+        //entrada
         base = Double.parseDouble(JOptionPane.showInputDialog("Digite a base:"));
         altura = Double.parseDouble(JOptionPane.showInputDialog("Digite a altura:"));
+        //processamento
         area = (base * altura)/2.0;
+        //saida
         JOptionPane.showMessageDialog(null, "a area é " + area);
         
     }
